503aa54444f653f0edccc9ecbec64890e8cd71b0
Sid <sidalbertok@gmail.com>
2025-01-15 14:20:39 -0300

    Update dev.js

--- defalura/dev.js
+++ defalura/dev.js
@@ -1,6 +1,6 @@
 function reb(){
     const rebinfo = document.getElementById("e").value  
-    document.getElementById("sd").innerHTML = `Ola ${rebinfo} , para verificar seu amigo secreto click no botão`;
+    document.getElementById("sd").innerHTML = `Ola ${rebinfo} , para verificar seu amigo secreto click no botão de Sorteio`;
 }
 
 function gera(){
