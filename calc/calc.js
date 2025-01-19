f0d7a1349925af17e12c3c4d4a7017877aafd4ae
Sid <sidalbertok@gmail.com>
2025-01-19 08:36:54 -0300

    Update calc.js

--- calc/calc.js
+++ calc/calc.js
@@ -7,16 +7,15 @@ function receb(){
         const tempotrampo = tempo*30
         const res = valor/tempotrampo
         const fgts = valor*0.08
-        const alm = valor*0.01
         const trans = valor*0.08
-        const desc = trans - alm - 80
+        const desc = valor - trans - alm - 80
         const salbruto = valor
 
         document.getElementById("res4").innerHTML = `Salario Bruto R$:${salbruto}`  
         document.getElementById("res1").innerHTML = `Valor por Hora R$:${res.toFixed(2)}` 
         document.getElementById("res2").innerHTML = `FGTS R$:${fgts.toFixed(2)}`  
-        document.getElementById("res3").innerHTML = `Descontos do salario R$:${desc.toFixed(2)}`  
-        document.getElementById("res5").innerHTML = `Total de Horas:${tempotrampo}`  
+        document.getElementById("res3").innerHTML = `Total Liquido com descontos R$:${desc.toFixed(2)}`  
+        document.getElementById("res5").innerHTML = `Total de Horas Trabalhadas:${tempotrampo}`  
         
     }else{
         document.getElementById("res1").innerHTML = "Por Favor insira um valor valido"
