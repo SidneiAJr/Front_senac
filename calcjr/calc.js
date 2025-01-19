c5f82889848dfbfedaff9b7ca302625a9e233d14
Sid <sidalbertok@gmail.com>
2025-01-19 09:03:57 -0300

    Create calc.js

---
+++ calcjr/calc.js
@@ -0,0 +1,14 @@
+function reb(){
+    const rec = document.getElementById("valorIni").value
+    const rec2 = document.getElementById("rend").value
+     
+    const calc = (rec2*100)/rec
+
+
+    if(rec&&isNaN(rec)||rec2&&isNaN(rec2)){
+        document.getElementById("res1").innerHTML = `O Juros atual é %${calc.toFixed(2)}` 
+    }else{
+        document.getElementById("res1").innerHTML = "Favor Inserir o valor correto" 
+    }
+    document.getElementById("res1").innerHTML = "Por Favor insira um valor valido"
+    }
