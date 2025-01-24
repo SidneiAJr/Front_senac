ad3d34bfea2dc0e2f4a196f963b87c52d5c3e8fe
Sid <sidalbertok@gmail.com>
2025-01-24 12:10:49 -0300

    Create cos.js

---
+++ hr/cos.js
@@ -0,0 +1,12 @@
+function conv(){
+    const n = document.getElementById("min").value
+    const res = n*60
+    const seg = n*3600
+    if(!isNaN(n)){
+    document.getElementById("res").innerHTML=`A conversão em Minutos será ${res}`
+    document.getElementById("res2").innerHTML=`A conversão em Segundos será ${seg}`
+    }else{
+    document.getElementById("res").innerHTML=`Favor informar um valor valido`
+    }
+    
+}
