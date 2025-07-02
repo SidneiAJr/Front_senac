697bf0c49a6307234f61d117e62c8cc3adf97020
Sid Jr <sidalbertok@gmail.com>
2025-07-02 17:17:03 -0300

    Create questions.js

---
+++ Questionador/questions.js
@@ -0,0 +1,9 @@
+function soft() {
+    let pergunta = prompt("Qual sua Softskill preferida?")
+    document.getElementById("peg").innerHTML = `Sua resposta é: ${pergunta}. Está certo? Obrigado por usar o questionador dos guri.`
+}
+
+function hard() {
+    let pergunta2 = prompt("Qual Hard Skill você gosta?")
+    document.getElementById("peg").innerHTML = `Sua resposta é: ${pergunta2}. Está certo? Obrigado por usar o questionador dos guri.`
+}
