d13f096a06dd0fe5f9d3d75af399ccf3103d69a9
Sid Jr <sidalbertok@gmail.com>
2025-07-02 17:21:47 -0300

    Update questions.js

--- Questionador/questions.js
+++ Questionador/questions.js
@@ -1,9 +1,13 @@
 function soft() {
     let pergunta = prompt("Qual sua Softskill preferida?")
+    let pergunta3 = prompt("Qual sua Softskill preferida?")
     document.getElementById("peg").innerHTML = `Sua resposta é: ${pergunta}. Está certo? Obrigado por usar o questionador dos guri.`
+    document.getElementById("peg").innerHTML = `Sua resposta é: ${pergunta3}. Está certo? Obrigado por usar o questionador dos guri.`
 }
 
 function hard() {
     let pergunta2 = prompt("Qual Hard Skill você gosta?")
+    let pergunta4 = prompt("Qual Hard Skill você gosta?")
     document.getElementById("peg2").innerHTML = `Sua resposta é: ${pergunta2}. Está certo? Obrigado por usar o questionador dos guri.`
+    document.getElementById("peg").innerHTML = `Sua resposta é: ${pergunta4}. Está certo? Obrigado por usar o questionador dos guri.`
 }
