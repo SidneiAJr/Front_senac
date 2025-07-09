0d0ddac639531118cc24053b5c11c0cb88d96c47
Sid Jr <sidalbertok@gmail.com>
2025-07-09 19:57:18 -0300

    Add files via upload

---
+++ Calculadora_Reajuste/calculator.js
@@ -0,0 +1,19 @@
+function calculador(){
+    let valor1 = Number(document.getElementById("val_atual").value)
+    let valor2 = Number(document.getElementById("val_reajust").value)
+    let reajuste = 0
+    let Percentual = 0
+
+    if(!isNaN(valor1)||!isNaN(valor2)){
+        reajuste = valor2 - valor1
+        Percentual = (valor1/valor2)*100
+        if(Percentual>50){
+            document.getElementById("res").innerHTML=`
+            Diferença R$: ${reajuste} <br>
+            Percentual de Reajuste: ${Percentual.toFixed(2)} %
+            `
+        }else{
+             document.getElementById("res").innerHTML=`Melhore!!`
+        }
+    }
+}
