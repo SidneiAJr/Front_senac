fa644b0e4862b5ecbe8dca8b3112223ba8cd9eb5
Sid Jr <sidalbertok@gmail.com>
2025-07-10 19:27:50 -0300

    Update calculator.js

--- Calculadora_Cotas/calculator.js
+++ Calculadora_Cotas/calculator.js
@@ -3,24 +3,29 @@ function calculador(){
     let preco_medio = parseFloat(document.getElementById("preco_cota").value)
     let provento = parseFloat(document.getElementById("prove").value)
     let disponivel_aplicar = parseFloat(document.getElementById("disp").value)
+    let salario_minimo_bruto = parseFloat(document.getElementById("minimo").value)
     let quant_compras = 0 
     let totalCotas = 0 
     let proventototal = 0
     let proventoanterior = 0
-    let percentual_aumento_provento = 0
+    let salario_minimo = 0
+    let cotas_salario = 0
+
 
     if(!isNaN(cotas_atuais)||!isNaN(preco_medio)||!isNaN(provento)){
         quant_compras = disponivel_aplicar/preco_medio
         totalCotas =quant_compras+cotas_atuais
         proventototal = totalCotas*provento
         proventoanterior = provento
-        percentual_aumento_provento = (proventoanterior/proventototal)*100
+        salario_minimo = salario_minimo_bruto/provento
+        cotas_salario = salario_minimo*preco_medio
             document.getElementById("res").innerHTML=`
             Quantidade de Cotas para Aqusição: ${quant_compras.toFixed(0)} <br>
             Total de Cotas: ${totalCotas.toFixed(0)}<br>
             Provento Anterior R$: ${proventoanterior}<br>
             Provento Atual R$: ${proventototal.toFixed(2)}<br>
-            Percentual de Aumento do provento ${proventototal.toFixed(2)}%<br>
+            Salario Minimo em cotas :${salario_minimo.toFixed(0)}<br>
+            Custo para ter um salario Minimo em cotas ${cotas_salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>
             `
         }else{
              document.getElementById("res").innerHTML=`Melhore!!`
