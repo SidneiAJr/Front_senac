dd8abc6133dac4334221ea91c60fbc68da4eb47f
Sid Jr <sidalbertok@gmail.com>
2026-05-15 20:18:25 -0300

    Update index.js

--- Frotend-senac/index.js
+++ Frotend-senac/index.js
@@ -1,16 +1,14 @@
-const fundo = document.getElementById('menu');
+const fundo_menu = document.getElementById('menu');
 const titulos = document.querySelector('a');
-const icones = document.getElementById('icons');
 const textos = document.querySelector('h1');
 const textos2 = document.querySelector('h2');
 const textos3 = document.querySelector('h4');
+const corpo = document.querySelector('body');
 
 function modonoturno(){
-   fundo.style.background = "rgba(23, 19, 21, 0.8)";
-   fundo.style.opacity = "90%";
+   fundo_menu.style.background = "rgba(23, 19, 21, 0.8)";
+   fundo_menu.style.opacity = "90%";
    titulos.style.fontWeight = "bold";
-   icones.style.background = "white";
-   icones.style.borderRadius = "20px";
    textos.style.fontSize = "50px";
    textos2.style.fontSize = "50px";
    textos2.style.color = "black";
@@ -18,6 +16,7 @@ function modonoturno(){
    textos3.style.fontSize = "50px";
    textos3.style.color = "black";
    textos3.style.fontStyle = "bolder";
+   document.body.style.background = "url(https://images.unsplash.com/photo-1760458365440-6931ab0ae9b6?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
 
 }
 
